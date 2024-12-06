075163df24163785550902514a75b7a23d9d7733
Pradeep Rajendran <rajendran@porziols.com>
2024-12-05 21:17:21 -0500

    Working with ev variables

--- src/translations/index.ts
+++ src/translations/index.ts
@@ -1,12 +1,19 @@
 import { ITranslations } from "../types/ITranslations";
+import { GoodbyeMessage } from "./messages/goodbye";
+import { LoginMessage } from "./messages/login";
+import { LogoutMessage } from "./messages/logout";
+import { SaveSuccessMessage } from "./messages/save-success";
+import { SuccessMessage } from "./messages/success";
+import { WelcomeMessage } from "./messages/welcome";
 
-export const locales = ['en-us', 'fr', 'de'] as const;
+export const locales = ['en-us', 'fr', 'de', 'in', 'jp'] as const;
 export type Locales = typeof locales[number];
 
 export const translations: ITranslations<Locales>[] = [
-    { key: 'WELCOME-LABEL', 'en-us': 'Welcome', fr: 'Bienvenue', de: 'Willkommen' },
-    { key: 'GOODBYE-LABEL', 'en-us': 'Goodbye', fr: 'Au revoir', de: 'Verabschiedung' },
-    
-    // ❌ Missing 'de' will trigger a TypeScript error
-    // { key: 'Goodbye', 'en-us': 'Goodbye', fr: 'Au revoir' },
+    GoodbyeMessage,
+    LoginMessage,
+    LogoutMessage,
+    SaveSuccessMessage,
+    SuccessMessage,
+    WelcomeMessage
 ];
